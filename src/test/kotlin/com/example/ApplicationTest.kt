146b5b6450edfae43b34267fd662843ed32ee144
SvetlanaMelekhova <s.melekhova@dinamicadigital.io>
2022-05-07 12:24:20 +0300

    create test for root endpoint

---
+++ src/test/kotlin/com/example/ApplicationTest.kt
@@ -0,0 +1,27 @@
+package com.example
+
+import io.ktor.application.Application
+import io.ktor.http.HttpMethod
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.handleRequest
+import io.ktor.server.testing.withTestApplication
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class ApplicationTest {
+    @Test
+    fun testRoot() {
+        withTestApplication(moduleFunction = Application::module) {
+            handleRequest(HttpMethod.Get, "/").apply {
+                assertEquals(
+                    expected = HttpStatusCode.OK,
+                    actual = response.status()
+                )
+                assertEquals(
+                    expected = "Welcome to Boruto API!",
+                    actual = response.content
+                )
+            }
+        }
+    }
+}
